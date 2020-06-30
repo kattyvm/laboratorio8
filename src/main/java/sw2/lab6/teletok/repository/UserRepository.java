41aeada240064d05596b127f960af1c1986fa179
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-06-29 20:59:24 -0500

    token

--- src/main/java/sw2/lab6/teletok/repository/UserRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sw2.lab6.teletok.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByUsername(String username);
+    public Optional<User> findByUsername(String username);
 }
