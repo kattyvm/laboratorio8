d0f64798a0a65ff9f11f0c8d84bdd5523b90fff0
allisonvp <a.velardep@pucp.edu.pe>
2020-06-29 20:37:35 -0500

    commentPost

--- src/main/java/sw2/lab6/teletok/config/CommentWebService.java
+++ src/main/java/sw2/lab6/teletok/config/CommentWebService.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import sw2.lab6.teletok.dto.CommentPost;
 import sw2.lab6.teletok.entity.Post;
 import sw2.lab6.teletok.entity.PostComment;
